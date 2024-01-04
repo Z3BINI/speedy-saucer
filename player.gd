3958de536170db2269a3d0aea26ae5b0d8b5dcdd
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-04 17:10:35 +0000

    Added Player movement
    apply_force is used towards the direction the user wants to go.

--- player.gd
+++ player.gd
@@ -1,1 +1,14 @@
 extends RigidBody2D
+
+var FORCE = 1000
+	
+func _physics_process(delta):
+	
+	if Input.is_action_pressed('up'):
+		apply_force(Vector2(0, -FORCE))
+	if Input.is_action_pressed('down'):
+		apply_force(Vector2(0, FORCE))
+	if Input.is_action_pressed('left'):
+		apply_force(Vector2(-FORCE, 0))
+	if Input.is_action_pressed('right'):
+		apply_force(Vector2(FORCE, 0))
